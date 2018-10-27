592961dbda8bac236beb4215e9e96c796890e48f
dkbrookie <matthew@dkbinnovative.net>
2018-10-27 01:55:35 -0500

    Update 1803Update.ps1

--- 1803Update.ps1
+++ 1803Update.ps1
@@ -1,73 +1,64 @@
 #region functions
-Function Get-Tree($Path,$Include='*'){
+Function Get-Tree($Path,$Include='*') {
   @(Get-Item $Path -Include $Include -Force) +
     (Get-ChildItem $Path -Recurse -Include $Include -Force) | Sort PSPath -Descending -Unique
 }
 
-Function Remove-Tree($Path,$Include='*'){
+Function Remove-Tree($Path,$Include='*') {
   Get-Tree $Path $Include | Remove-Item -Force -Recurse
 }
 #endregion functions
 
 
 #region checkOSInfo
-If([System.Environment]::OSVersion.Version.Major -ne 10){
+If([System.Environment]::OSVersion.Version.Major -ne 10) {
   Write-Output "Your version of Windows does not support the 1803 upgrade. Exiting script."
   Break
 }
 
 $osBuild = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ReleaseId).ReleaseId
-If($osBuild -ge 1803){
+If($osBuild -ge 1803) {
   Write-Output "Your machine already has this patch installed! Exiting script."
   Break
 }
 
-If($env:PROCESSOR_ARCHITECTURE -eq 'AMD64'){
-  $osVer = 'x64'
-} Else {
-  $osVer = 'x86'
+Try {
+  If((Get-WmiObject win32_operatingsystem | Select-Object -ExpandProperty osarchitecture) -eq '64-bit') {
+    $osVer = 'x64'
+    $ltServFile = 3927745135
+  } Else {
+    $ltServFile = 2942440134
+    $osVer = 'x86'
+  }
+} Catch {
+  Write-Error 'Unable to determine OS architecture'
+  Return
 }
 #endregion checkOSInfo
 
 
 #region dirChecks
-$ltDir = "C:\Windows\LTSvc"
-$packageDir = "$ltDir\packages"
-$osDir = "$packageDir\OS"
-$1803Dir = "$osDir\win10.1803"
-
-If(!$packageDir){
-  New-Item -ItemType Directory -Path $packageDir
-}
-
-If(!$osDir){
-  New-Item -ItemType Directory -Path $osDir
-}
-
-If(!$1803Dir){
-  New-Item -ItemType Directory -Path $1803Dir
+Try {
+  $1803Dir = "$env:windir\LTSvc\packages\OS\win10.1803"
+  If(!(Test-Path $1803Dir)) {
+    New-Item -ItemType Directory -Path $1803Dir
+  }
+} Catch {
+  Write-Error 'Failed to create folder'
+  Return
 }
 #endregion dirChecks
 
 
 #region fileChecks
-If($osVer -eq 'x64'){
-  $ltServFile = 3927745135
-}
-
-If($osVer -eq 'x86'){
-  $ltServFile = 2942440134
-}
-
 $zipPath = "$1803Dir\Pro$osVer.1803.zip"
 $checkZip = Test-Path $zipPath -PathType Leaf
-
-If($checkZip){
+If($checkZip) {
   $clientFile = Get-Item $zipPath
-  If($ltServFile -gt $clientFile.Length){
+  If($ltServFile -gt $clientFile.Length) {
     Remove-Tree -Path $1803Dir
     $checkFile = Test-Path $zipPath -PathType Leaf
-    If(!$checkFile){
+    If(!$checkFile) {
       $status = 'Download'
       Write-Output "The existing installation files for the 1803 update were incomplete or corrupt. Deleted existing files and now starting a new download."
     }
@@ -86,3 +77,13 @@ Else{
   Write-Output "The required files to install the 1803 update are not present, downloading required files now."
 }
 #endregion fileChecks
+
+
+#region install1803
+If($status -eq 'Download') {
+  ##downloadhere
+}
+ElseIf($status -eq 'Install') {
+  ##installhere
+}
+#endregion install1803
