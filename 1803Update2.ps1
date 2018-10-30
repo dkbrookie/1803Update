830cca0eae56e75e36a783dd95404035564a5d51
dkbrookie <matthew@dkbinnovative.net>
2018-10-30 16:36:30 -0500

    Removed duplicate version

--- 1803Update2.ps1
+++ 1803Update2.ps1
@@ -7,10 +7,35 @@ Function Get-Tree($Path,$Include='*') {
 Function Remove-Tree($Path,$Include='*') {
   Get-Tree $Path $Include | Remove-Item -Force -Recurse
 }
+
+Function Install-withProgress{
+  $localFolder= (Get-Location).path
+  $process = Start-Process -FilePath "$localFolder\Installer.exe" -ArgumentList "/silent /accepteula" -PassThru
+  For($i = 0; $i -le 100; $i = ($i + 1) % 100) {
+    Write-Progress -Activity "Installer" -PercentComplete $i -Status "Installing"
+    Start-Sleep -Milliseconds 100
+    If ($process.HasExited) {
+        Write-Progress -Activity "Installer" -Completed
+        Break
+    }
+  }
+}
 #endregion functions
 
 
+
 #region checkOSInfo
+$rbCheck1 = Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore
+$rbCheck2 = Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore
+$rbCheck3 = Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore
+
+If($rbCheck1 -ne $Null -or $rbCheck2 -ne $Null -or $rbCheck3 -ne $Null){
+    Write-Output "This system is pending a reboot, unable to proceed. Please restart your computer and try again."
+    Break
+} Else {
+    Write-Output "Automation has verified there is no reboot pending."
+}
+
 $desktop = [Environment]::GetFolderPath("Desktop")
 $logFile = "$desktop\1803 Upgrade Status.txt"
 $dateTime = Get-Date
@@ -42,26 +67,22 @@ Try {
 #endregion checkOSInfo
 
 
+#region fileChecks
 $1803Dir = "$env:windir\LTSvc\packages\OS\win10.1803"
 $1803Zip = "$1803Dir\Pro$osVer.1803.zip"
 $7zip = "$1803Dir\7za.exe"
 $automate1803URL = "https://support.dkbinnovative.com/labtech/Transfer/OS/Windows10/Pro$osVer.1803.zip"
 $automate7zipURL = "https://support.dkbinnovative.com/labtech/Transfer/OS/Windows10/7za.exe"
 
-
-#region dirChecks
 Try {
   If(!(Test-Path $1803Dir)) {
-    New-Item -ItemType Directory -Path $1803Dir
+    New-Item -ItemType Directory -Path $1803Dir | Out-Null
   }
 } Catch {
   Write-Error "Failed to create the following folder: $1803Dir" | Out-File $logFile -Append
   Return
 }
-#endregion dirChecks
 
-
-#region fileChecks
 $checkZip = Test-Path $1803Zip -PathType Leaf
 If($checkZip) {
   If($servFile -gt (Get-Item $1803Zip).Length) {
@@ -92,11 +113,10 @@ Else {
 If($status -eq 'Download') {
   Try {
     Start-BitsTransfer -Source $automate1803URL -Destination $1803Zip
-    #IWR -Uri $automate1803URL -Outfile $1803Zip
     If($servFile -gt (Get-Item $1803Zip).Length) {
       Write-Error "The downloaded size of $1803Zip does not match the server version, unable to install the update." | Out-File $logFile -Append
     } Else {
-      Write-Output 'Successfully downloaded the 1803 update! Your system is currently unpacking the setup files, then will begin the installation process.' | Out-File $logFile -Append
+      Write-Output 'Successfully downloaded the 1803 update!' | Out-File $logFile -Append
       $status = 'Unzip'
     }
   } Catch {
@@ -116,12 +136,24 @@ Try {
   }
   ##Install
   If($status -eq 'Install') {
-    Write-Warning 'The 1803 upgrade installation has now been started silently in the background. No action from you is required, but please note a reboot will be reqired during the installation prcoess. It is highly recommended you save all of your open files!' | Out-File $logFile -Append
-    Start-Process -FilePath "$1803Dir\Pro$osVer.1803\setup.exe" -ArgumentList "/auto upgrade" "/quiet" -Wait
+    Write-Output 'The 1803 upgrade installation has now been started silently in the background. No action from you is required, but please note a reboot will be reqired during the installation prcoess. It is highly recommended you save all of your open files!' | Out-File $logFile -Append
+    $localFolder= (Get-Location).path
+    $process = Start-Process -FilePath "$1803Dir\Pro$osVer.1803\setup.exe" -ArgumentList "/auto upgrade /quiet" -PassThru
+    For($i = 0; $i -le 100; $i = ($i + 1) % 100) {
+      Write-Progress -Activity "Installer" -PercentComplete $i -Status "Installing"
+      Start-Sleep -Milliseconds 100
+      If ($process.HasExited) {
+        Write-Progress -Activity "Installer" -Completed
+        Write-Output "Installation has completed, deleting setup files" | Out-File $logFile -Append
+        Remove-Tree -Path $1803Dir
+        Write-Output "1803 upgrade process complete!"
+        Break
+      }
+    }
   } Else {
       Write-Error "Could not find a known status of the var Status. Output: $status" | Out-File $logFile -Append
   }
 } Catch {
-  Write-Error 'Setup ran into an inssue while attempting to install the 1803 upgrade.'
+  Write-Error 'Setup ran into an issue while attempting to install the 1803 upgrade.'
 }
   #endregion download/install
